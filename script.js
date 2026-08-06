b8642a1cdfb5f9656b90921355b2a37a0e66c433
Cotoc Ianis Andrei <ianis.cotoc@gmail.com>
2026-08-07 00:03:25 +0300

    page changes view templates etc

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ if (yearNode) {
 
 const navToggle = document.querySelector('.nav-toggle');
 const mainNav = document.querySelector('.main-nav');
+const siteHeader = document.querySelector('.site-header');
+let hideTimer;
 
 if (navToggle && mainNav) {
   navToggle.addEventListener('click', () => {
@@ -19,3 +21,56 @@ if (navToggle && mainNav) {
     });
   });
 }
+
+if (siteHeader) {
+  const showHeader = () => {
+    siteHeader.classList.remove('is-hidden');
+    siteHeader.classList.add('is-visible');
+  };
+
+  const hideHeader = () => {
+    if (window.scrollY > 8) {
+      siteHeader.classList.remove('is-visible');
+      siteHeader.classList.add('is-hidden');
+    }
+  };
+
+  const scheduleHide = () => {
+    clearTimeout(hideTimer);
+    hideTimer = setTimeout(() => {
+      hideHeader();
+    }, 220);
+  };
+
+  showHeader();
+
+  window.addEventListener('scroll', () => {
+    showHeader();
+
+    if (window.scrollY > 8) {
+      scheduleHide();
+    }
+  }, { passive: true });
+
+  window.addEventListener('wheel', () => {
+    showHeader();
+  }, { passive: true });
+
+  window.addEventListener('touchmove', () => {
+    showHeader();
+  }, { passive: true });
+
+  document.addEventListener('mousemove', (event) => {
+    if (event.clientY <= 100) {
+      showHeader();
+    }
+  });
+
+  siteHeader.addEventListener('mouseenter', showHeader);
+  siteHeader.addEventListener('mouseleave', () => {
+    if (window.scrollY > 8) {
+      scheduleHide();
+    }
+  });
+}
+
